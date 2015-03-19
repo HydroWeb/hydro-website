f5f6e65dd7bc19a75352da3b9e50f7acc35bea44
Tal Meser <tal@hydroweb.co.nz>
2015-03-19 13:14:47 +1300

    ORM + Models config

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+/**
+ * Built-in Log Configuration
+ * (sails.config.log)
+ *
+ * Configure the log level for your app, as well as the transport
+ * (Underneath the covers, Sails uses Winston for logging, which
+ * allows for some pretty neat custom transports/adapters for log messages)
+ *
+ * For more information on the Sails logger, check out:
+ * http://sailsjs.org/#/documentation/concepts/Logging
+ */
+
+module.exports.orm = {
+
+  _hookTimeout: 60000 // I used 60 seconds as my new timeout
+  
+};
